4589b10699f7b4139e108a2e180618e02447e8c6
soundarya1627 <contactsandy9@gmail.com>
2021-08-09 17:16:37 -0800

    add sample method program

---
+++ csharpbasics/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharpbasics
+{
+    public class Employee
+    {
+        int emp_ID;
+        string emp_Name = null;
+        public void SetEmployeeID(int Id)
+        {
+            emp_ID = Id;
+        }
+        public void SetEmployeeName(string name)
+            {
+            emp_Name = name;
+        }
+           public void GetEmployeeDetails()
+        {
+            Console.WriteLine(" EMP ID: " + emp_ID);
+            Console.WriteLine("EMP NAME: " + emp_Name);
+        }
+          
+    }
+}
---
+++ csharpbasics/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace csharpbasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to c# Bsics");
+
+
+            Employee emp_1 = new Employee();
+            emp_1.SetEmployeeID(12345);
+            emp_1.SetEmployeeName("upeksha");
+            emp_1.GetEmployeeDetails();
+        }
+    }
+}
